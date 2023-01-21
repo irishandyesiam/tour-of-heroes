05afc2d24e3cd51909b89ac961cbbba0fd62e01b
irishandyesiam <nathan.irish@gmail.com>
2023-01-21 16:04:14 -0600

    HttpClientModuleInMomoryWebApiModule to intercept and return simulated server responses.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,17 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { MessagesComponent } from './messages/messages.component';
-
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 import { AppRoutingModule } from './app-routing.module';
+HttpClientModule,
 
+// The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+// and returns simulated server responses.
+// Remove it when a real server is ready to receive requests.
+HttpClientInMemoryWebApiModule.forRoot(
+  InMemoryDataService, { dataEncapsulation: false }
+)
 @NgModule({
   imports: [
     BrowserModule,
