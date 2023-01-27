acb56a4523ac7e1c4030d0885085adb7c9115f93
irishandyesiam <nathan.irish@gmail.com>
2023-01-27 11:33:15 -0600

    Extended observable result with pipe() method + catchError() operator + handleError().

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -23,6 +23,9 @@ private log(message: string) {
 /** GET heroes from the server */
 getHeroes(): Observable<Hero[]> {
   return this.http.get<Hero[]>(this.heroesUrl)
+    .pipe(
+      catchError(this.handleError<Hero[]>('getHeroes', []))
+    );
 }
 
   getHero(id: number): Observable<Hero> {
@@ -32,4 +35,25 @@ getHeroes(): Observable<Hero[]> {
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(hero);
   }
+}
+
+/**
+ * Handle Http operation that failed.
+ * Let the app continue.
+ *
+ * @param operation - name of the operation that failed
+ * @param result - optional value to return as the observable result
+ */
+private handleError<T>(operation = 'operation', result?: T) {
+  return (error: any): Observable<T> => {
+
+    // TODO: send the error to remote logging infrastructure
+    console.error(error); // log to console instead
+
+    // TODO: better job of transforming error for user consumption
+    this.log(`${operation} failed: ${error.message}`);
+
+    // Let the app keep running by returning an empty result.
+    return of(result as T);
+  };
 }
